89610bfe1be491e744528ab8a7ddb3bc721ae2d0
BrunaMello <brunacrespomello@gmail.com>
2024-11-19 14:24:11 +0000

    Initial Commit

---
+++ HabitHeroTests/HabitHeroTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitHeroTests.swift
+//  HabitHeroTests
+//
+//  Created by Bruna Bianca Crespo Mello on 19/11/2024.
+//
+
+import Testing
+@testable import HabitHero
+
+struct HabitHeroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
